632928681c4a71da1c3bd278c182648ca6109dc9
FBI911 <93187995+FBI124@users.noreply.github.com>
2023-06-15 19:18:15 +0700

    Update letters.js

--- LETTERS/letters.js
+++ LETTERS/letters.js
@@ -1,1 +1,19 @@
+//sending emails to the email
 
+	$('form').submit(function(e) {
+		e.preventDefault();
+		if (!$(this).valid()) {
+			return;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find('input').val("");
+			$('#consultation, #order').fadeOut();
+            $('.overlay, #application').fadeIn();
+			$('form').trigger('reset');
+		})
+		return false;
+	});
