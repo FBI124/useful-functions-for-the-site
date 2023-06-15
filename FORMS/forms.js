54fcc669473bd00adba3a217cb5bc9ef6afb9bf5
FBI911 <93187995+FBI124@users.noreply.github.com>
2023-06-15 17:59:31 +0700

    Update forms.js

--- FORMS/forms.js
+++ FORMS/forms.js
@@ -1,1 +1,39 @@
+//customization of the form
 
+	function valideForms(your class) {
+		$(your class).validate({
+			rules: {
+				name: 'required',
+				phone: {
+					minlength: your number,
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "your text",
+				phone: {
+					required: "your text",
+				},
+				email: {
+				  required: "your text",
+				  email: "your text"
+				}
+			}	
+		});
+	};
+
+  //red cross to close the form
+
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #application, #order').fadeOut();
+	});
+
+	//form validation
+
+	valideForms('#your class (selector)');
+	valideForms('#your class (selector)');
+	valideForms('#your class (selector)');
