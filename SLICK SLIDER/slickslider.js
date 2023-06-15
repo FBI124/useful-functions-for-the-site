db57a681b2ea8ea03b835e49f2fce6c0fce887d6
FBI911 <93187995+FBI124@users.noreply.github.com>
2023-06-15 18:05:13 +0700

    Update slickslider.js

--- SLICK SLIDER/slickslider.js
+++ SLICK SLIDER/slickslider.js
@@ -1,1 +1,20 @@
-
+	$('your class').slick({
+		speed: 1300,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		fade: true,
+		speed: 900,
+		cssEase: 'linear',
+		prevArrow: '<button type="button" class="slick-prev"><img src="your way"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="your way"></button>',
+		dots: '<button type="button" class="slick-dots"></button>', // for adaptaton
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+				  arrows: false,
+				  dots: true
+				}
+			}
+		]
+	});	
