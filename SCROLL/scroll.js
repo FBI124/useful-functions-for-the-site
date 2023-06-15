7f65c76a975bcede90ef46eba40addd102c52fb6
FBI911 <93187995+FBI124@users.noreply.github.com>
2023-06-15 17:47:26 +0700

    Update scroll.js

--- SCROLL/scroll.js
+++ SCROLL/scroll.js
@@ -1,3 +1,13 @@
+	//arrow to scroll up
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop () > 1500) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
 	//smooth scroll on the website when you clicking on the arrow
 
 	$("a").on('click', function(event) {
