7cc0e2066b4ce9d45ac0cf6d3998619ab09d3f26
FBI911 <93187995+FBI124@users.noreply.github.com>
2023-06-15 17:45:30 +0700

    Update scroll.js

--- SCROLL/scroll.js
+++ SCROLL/scroll.js
@@ -1,1 +1,17 @@
+	//smooth scroll on the website when you clicking on the arrow
 
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+		  event.preventDefault();
+		  var hash = this.hash;
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+			window.location.hash = hash;
+		  });
+		}
+	});
+
+	//animation of reviews (wow.js + animate.css)
+
+	new WOW().init();
