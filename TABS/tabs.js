c2f24f8449d54d00a04b04e64f23762ca65d715c
FBI911 <93187995+FBI124@users.noreply.github.com>
2023-06-15 19:02:34 +0700

    Create tabs.js

---
+++ TABS/tabs.js
@@ -0,0 +1,36 @@
+//script for tabs: packaging each content into a separate tab
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	//script for switching information in the card ("MORE" button)
+
+	$('.catalog-item__link').each(function(i) {
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+		})
+	});
+
+	//script for switching information in the card back ("BACK" button)
+
+	$('.catalog-item__back').each(function(i) {
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+		})
+	});
+
+//script for the name of the product (heart rate monitor that was clicked on) display in the modal window
+
+$('.catalog-item__btn').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn();
+		})
+	});
